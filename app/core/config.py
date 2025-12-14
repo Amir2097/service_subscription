63e76f331e4c9546f4565e7078fc6f1e53c20688
Amir2097 <dautov.amir2013@yandex.ru>
2025-12-15 01:09:27 +0500

    Добавление еще первоначальных настроек, касается base and config.py

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=f"{BASE_DIR}/.env",  # Указываем файл с переменными
--- app/db/base.py
+++ app/db/base.py
@@ -7,3 +7,28 @@ from app.core.config import settings
 class Base(DeclarativeBase):
     pass
 
+
+# Создаем async engine
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=False,  # SQL-логирование (полезно для дебага)
+    future=True
+)
+
+
+# SessionLocal — фабрика сессий
+async_session_maker = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False,
+)
+
+
+# Dependency для FastAPI
+async def get_db():
+    async with async_session_maker() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
